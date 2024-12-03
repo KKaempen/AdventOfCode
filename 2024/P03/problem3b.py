cf1eb784696b44087987b9870ac5cee0cbe0dcaf
KKaempen <knack@flash.net>
2024-12-03 00:56:31 -0500

    Even better code

--- 2024/P03/problem3b.py
+++ 2024/P03/problem3b.py
@@ -6,19 +6,19 @@ with open("problem3.txt", 'r') as f:
 
 total = 0
 do = True
-for line in data:
-    regex = re.compile('mul\(\d\d?\d?,\d\d?\d?\)|do\(\)|don\'t\(\)')
+line = ''.join(data)
+regex = re.compile('mul\(\d\d?\d?,\d\d?\d?\)|do\(\)|don\'t\(\)')
 
-    matches = regex.findall(line)
-    for match in matches:
-        if match == 'do()':
-            do = True
-        elif match == 'don\'t()':
-            do = False
-        elif do:
-            nums = match[4: -1].split(',')
-            num1 = int(nums[0])
-            num2 = int(nums[1])
-            total += num1 * num2
+matches = regex.findall(line)
+for match in matches:
+    if match == 'do()':
+        do = True
+    elif match == 'don\'t()':
+        do = False
+    elif do:
+        nums = match[4: -1].split(',')
+        num1 = int(nums[0])
+        num2 = int(nums[1])
+        total += num1 * num2
 
 print(total)
