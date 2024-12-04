8154805c7918d5781c6e2511cf2d91dd9859274e
KKaempen <knack@flash.net>
2024-12-04 00:45:07 -0500

    Finish problem 4

--- 2024/P04/problem4a.py
+++ 2024/P04/problem4a.py
@@ -1,3 +1,16 @@
+import re
+
 data = []
 with open("problem4.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+n = len(data)
+m = len(data[0])
+data += [''.join([data[j][i] for j in range(n)]) for i in range(m)]
+data += [''.join([data[i + j][j] for j in range(min(m, n - i))]) for i in range(n)] + [''.join([data[i][j + i] for i in range(min(n, m - j))]) for j in range(1, m)]
+data += [''.join([data[n - 1 - (i + j)][j] for j in range(min(m, n - i))]) for i in range(n)] + [''.join([data[n - 1 - i][j + i] for i in range(min(n, m - j))]) for j in range(1, m)]
+
+regex = re.compile('(?=(XMAS|SAMX))')
+count = sum([len(regex.findall(line)) for line in data])
+
+print(count)
--- 2024/P04/problem4b.py
+++ 2024/P04/problem4b.py
@@ -1,3 +1,13 @@
 data = []
 with open("problem4.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+count = 0
+for i in range(len(data) - 2):
+    for j in range(len(data[i]) - 2):
+        l1 = data[i][j] + data[i + 1][j + 1] + data[i + 2][j + 2]
+        l2 = data[i][j + 2] + data[i + 1][j + 1] + data[i + 2][j]
+        if (l1 == 'MAS' or l1 == 'SAM') and (l2 == 'MAS' or l2 == 'SAM'):
+            count += 1
+
+print(count)
