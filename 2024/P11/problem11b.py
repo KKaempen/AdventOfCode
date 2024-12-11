7c1a4a4f13ee8bb438da0f7314a81473ac7c2486
KKaempen <knack@flash.net>
2024-12-11 14:27:32 -0500

    Improve solution by avoiding string-int conversions

--- 2024/P11/problem11b.py
+++ 2024/P11/problem11b.py
@@ -4,7 +4,7 @@ data = []
 with open("problem11.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
 
-stones = data[0].split()
+stones = [int(x) for x in data[0].split()]
 stone_dict = defaultdict(int)
 for stone in stones:
     stone_dict[stone] += 1
@@ -12,16 +12,19 @@ for stone in stones:
 for i in range(75):
     new_stone_dict = defaultdict(int)
     for stone, count in stone_dict.items():
-        if stone == '0':
-            new_stone_dict['1'] += count
-        elif len(stone) % 2 == 0:
-            half1 = str(int(stone[:len(stone) // 2]))
-            half2 = str(int(stone[len(stone) // 2:]))
-            new_stone_dict[half1] += count
-            new_stone_dict[half2] += count
+        if stone == 0:
+            new_stone_dict[1] += count
         else:
-            next_val = str(int(stone) * 2024)
-            new_stone_dict[next_val] += count
+            sqrt_nearest_10 = 1
+            while sqrt_nearest_10 * sqrt_nearest_10 <= stone:
+                sqrt_nearest_10 *= 10
+            if stone * 10 >= sqrt_nearest_10 * sqrt_nearest_10:
+                # Even length
+                half1, half2 = divmod(stone, sqrt_nearest_10)
+                new_stone_dict[half1] += count
+                new_stone_dict[half2] += count
+            else:
+                new_stone_dict[stone * 2024] += count
     stone_dict = new_stone_dict
 
 print(sum(stone_dict.values()))
