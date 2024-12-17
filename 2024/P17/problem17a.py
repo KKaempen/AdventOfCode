c84a5773bda1485b63a575a453049ffe51018daf
KKaempen <knack@flash.net>
2024-12-17 01:47:44 -0500

    Done with 17

--- 2024/P17/problem17a.py
+++ 2024/P17/problem17a.py
@@ -1,3 +1,39 @@
 data = []
 with open("problem17.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+regs_str, ops_str = '\n'.join(data).split('\n\n')
+regs = [int(x[12:]) for x in regs_str.split('\n')]
+ops = [int(x) for x in ops_str[9:].split(',')]
+
+inst_ptr = 0
+outs = []
+while inst_ptr < len(ops):
+    inst = ops[inst_ptr]
+    operand = ops[inst_ptr + 1]
+    combo_op = lambda x: regs[x - 4] if 4 <= x < 7 else x
+    if inst == 0:
+        operand = combo_op(operand)
+        regs[0] = regs[0] >> operand
+    elif inst == 1:
+        regs[1] = regs[1] ^ operand
+    elif inst == 2:
+        operand = combo_op(operand)
+        regs[1] = operand % 8
+    elif inst == 3:
+        if regs[0] != 0:
+            inst_ptr = operand - 2
+    elif inst == 4:
+        regs[1] = regs[1] ^ regs[2]
+    elif inst == 5:
+        operand = combo_op(operand)
+        outs.append(operand % 8)
+    elif inst == 6:
+        operand = combo_op(operand)
+        regs[1] = regs[0] >> operand
+    elif inst == 7:
+        operand = combo_op(operand)
+        regs[2] = regs[0] >> operand
+    inst_ptr += 2
+
+print(','.join([str(x) for x in outs]))
--- 2024/P17/problem17b.py
+++ 2024/P17/problem17b.py
@@ -1,3 +1,51 @@
 data = []
 with open("problem17.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+ops_str = data[-1]
+ops = [int(x) for x in ops_str[9:].split(',')]
+
+patterns = [[None for _ in range(8)] for _ in range(8)]
+for op in range(8):
+    for last_three in range(8):
+        shift = last_three ^ 7
+        c = last_three ^ op
+        a = [last_three & 1, (last_three & 2) >> 1, (last_three & 4) >> 2]
+        while len(a) < shift + 3:
+            a.append(-1)
+        for i in range(3):
+            mask = 1 if i == 0 else 2 if i == 1 else 4
+            to_place = (c & mask) >> i
+            idx = i + shift
+            if a[idx] >= 0 and a[idx] != to_place:
+                break
+            a[idx] = to_place
+        else:
+            patterns[op][last_three] = a
+
+possible_as = [[]]
+for i, op in enumerate(ops):
+    new_possible = []
+    shift = i * 3
+    next_chunks = patterns[op]
+    for a in possible_as:
+        for chunk in next_chunks:
+            if chunk is None:
+                continue
+            new_a = [x for x in a]
+            while len(new_a) < shift + len(chunk):
+                new_a.append(-1)
+            for i, v in enumerate(chunk):
+                if new_a[i + shift] >= 0 and chunk[i] >= 0 and new_a[i + shift] != chunk[i]:
+                    break
+                new_a[i + shift] = max(new_a[i + shift], chunk[i])
+            else:
+                new_possible.append(new_a)
+    possible_as = new_possible
+
+best_a = possible_as[0]
+total = 0
+for v in best_a[::-1]:
+    total *= 2
+    total += v
+print(total)
