5a6991ebe81e392adc46347cdfb9bbab78660c22
KKaempen <knack@flash.net>
2024-12-19 00:13:12 -0500

    Finished problem 19, solutions a little bit slow, need to improve DP

--- 2024/P19/problem19a.py
+++ 2024/P19/problem19a.py
@@ -1,3 +1,22 @@
 data = []
 with open("problem19.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+patterns, designs = '\n'.join(data).split('\n\n')
+patterns = set(patterns.split(', '))
+designs = designs.split('\n')
+
+count = 0
+for design in designs:
+    possible = [False for i in range(len(design) + 1)]
+    possible[0] = True
+    for i in range(len(design)):
+        if not possible[i]:
+            continue
+        for pattern in patterns:
+            if design[i:].startswith(pattern):
+                possible[i + len(pattern)] = True
+    if possible[-1]:
+        count += 1
+
+print(count)
--- 2024/P19/problem19b.py
+++ 2024/P19/problem19b.py
@@ -1,3 +1,21 @@
 data = []
 with open("problem19.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+patterns, designs = '\n'.join(data).split('\n\n')
+patterns = set(patterns.split(', '))
+designs = designs.split('\n')
+
+count = 0
+for design in designs:
+    possible = [0 for i in range(len(design) + 1)]
+    possible[0] = 1
+    for i in range(len(design)):
+        if not possible[i]:
+            continue
+        for pattern in patterns:
+            if design[i:].startswith(pattern):
+                possible[i + len(pattern)] += possible[i]
+    count += possible[-1]
+
+print(count)
