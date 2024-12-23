f805642ca96cb90faa77eb050bf24ddc88cd7064
Kieran Kaempen <knack@flash.net>
2024-12-22 23:27:34 -0600

    Finished day 23

--- 2024/P23/problem23a.py
+++ 2024/P23/problem23a.py
@@ -1,3 +1,22 @@
+from collections import defaultdict
+
 data = []
 with open("problem23.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+connections = defaultdict(set)
+for line in data:
+    n1, n2 = line.split('-')
+    connections[n1].add(n2)
+    connections[n2].add(n1)
+
+sets_of_3 = set()
+for n1 in connections:
+    for n2 in connections[n1]:
+        valid = connections[n1] & connections[n2]
+        for v in valid:
+            l = sorted([n1, n2, v])
+            t = tuple(l)
+            sets_of_3.add(t)
+
+print(len([x for x in sets_of_3 if any(v[0] == 't' for v in x)]))
--- 2024/P23/problem23b.py
+++ 2024/P23/problem23b.py
@@ -1,3 +1,32 @@
+from collections import defaultdict
+
 data = []
 with open("problem23.txt", 'r') as f:
     data = f.read().strip('\n').split('\n')
+
+connections = defaultdict(set)
+for line in data:
+    n1, n2 = line.split('-')
+    connections[n1].add(n2)
+    connections[n2].add(n1)
+
+def calc_max_clique(clique, conns, possible):
+    if len(possible) == 0:
+        return len(clique), clique
+    best_val, best_clique = 0, set()
+    to_iter = [p for p in possible]
+    for p in to_iter:
+        new_clique = clique | {p}
+        new_possible = conns[p] & possible
+        if len(new_clique) + len(new_possible) <= best_val:
+            continue
+        size, curr_clique = calc_max_clique(new_clique, conns, new_possible)
+        possible.remove(p)
+        if size > best_val:
+            best_val = size
+            best_clique = curr_clique
+    return best_val, best_clique
+
+_, clique = calc_max_clique(set(), connections, set(connections.keys()))
+password = ','.join(sorted([x for x in clique]))
+print(password)
